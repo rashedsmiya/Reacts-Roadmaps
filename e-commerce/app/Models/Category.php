66ca593ba9579f533b994f8a9f5759b5616074cb
Rashed Miya <132655219+rashedsmiya@users.noreply.github.com>
2025-10-08 21:32:32 +0600

    add

--- e-commerce/app/Models/Category.php
+++ e-commerce/app/Models/Category.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active',
+        'image',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
--- e-commerce/app/Models/Product.php
+++ e-commerce/app/Models/Product.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'image',
+        'on_sale',
+
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
